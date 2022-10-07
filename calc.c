7113dd5bf6fdf1a7be9308d247cbccfa3f9d4fcb
Flais <fenshu28@gmail.com>
2022-10-07 16:14:50 -0500

    firts commit

---
+++ calc.c
@@ -0,0 +1,24 @@
+#include <gtk/gtk.h>
+
+int main(){
+	ventana GtkWidget *;
+	Boton GtkWidget *;
+	
+	gtk_init (& arg,& arg);
+	
+	ventana = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	
+	g_signal_connect
+	
+	boton = gtk_button_new_with_label("Holas");
+	
+}
+
+void destruir (widget GtkWidget *, datos gpointer){
+	gtk_main_quit();
+}
+
+
+
+
+
